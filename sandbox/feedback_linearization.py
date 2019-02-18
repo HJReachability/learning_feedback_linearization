fixing signs and generating y

sandbox/feedback_linearization.py
@@ -52,25 +52,25 @@ class FeedbackLinearization(object):
         Initialize from a Dynamics object.
         This will encode the following control law:
 
-             ``` u = [ M1(x) + M2(x) ] * [ (w1(x) + w2(x)) + v ] + noise ```
+             ``` u = [ M1(x) + M2(x) ] * v + [ f1(x) + f2(x) ] + noise ```
 
-        where M1(x) and w1(x) are derived from modeled dynamics, and
-        M2(x) and w2(x) are torch.Tensor objects that we will learn with policy
+        where M1(x) and f1(x) are derived from modeled dynamics, and
+        M2(x) and f2(x) are torch.Tensor objects that we will learn with policy
         gradients.
         """
-        self._M1, self._w1 = dynamics.feedback_linearize()
+        self._M1, self._f1 = dynamics.feedback_linearize()
 
         # Create some PyTorch tensors for unmodeled dynamics terms.
         self._M2 = create_network(
             dynamics.xdim, dynamics.udim**2,
             num_layers, num_hidden_units, activation)
-        self._w2 = create_network(
+        self._f2 = create_network(
             dynamics.xdim, dynamics.udim,
             num_layers, num_hidden_units, activation)
 
         # Initialize weights.
         init_weights(self._M2)
-        init_weights(self._w2)
+        init_weights(self._f2)
 
         self._xdim = dynamics.xdim
         self._udim = dynamics.udim
@@ -84,10 +84,10 @@ class FeedbackLinearization(object):
 
         M = torch.from_numpy(self._M1(x)).float() + torch.reshape(
             self._M2(torch.from_numpy(x.flatten()).float()), (self._udim, self._udim))
-        w = torch.from_numpy(self._w1(x)).float() + torch.reshape(
-            self._w2(torch.from_numpy(x.flatten()).float()), (self._udim, 1))
+        f = torch.from_numpy(self._f1(x)).float() + torch.reshape(
+            self._f2(torch.from_numpy(x.flatten()).float()), (self._udim, 1))
 
-        return torch.mm(M, torch.from_numpy(v).float()) + w
+        return torch.mm(M, torch.from_numpy(v).float()) + f
 
     def noisy_feedback(self, x, v):
         """ Compute noisy version of u given x, v (np.arrays). """

sandbox/reinforce.py
@@ -29,8 +29,8 @@ class Reinforce(object):
         self._M2_optimizer = torch.optim.Adam(
             self._feedback_linearization._M2.parameters(),
             lr=self._learning_rate)
-        self._w2_optimizer = torch.optim.Adam(
-            self._feedback_linearization._w2.parameters(),
+        self._f2_optimizer = torch.optim.Adam(
+            self._feedback_linearization._f2.parameters(),
             lr=self._learning_rate)
 
     def run(self, plot=False):
@@ -136,7 +136,7 @@ class Reinforce(object):
 
             for param_group in self._M2_optimizer.param_groups:
                 param_group['lr'] = self._learning_rate
-            for param_group in self._w2_optimizer.param_groups:
+            for param_group in self._f2_optimizer.param_groups:
                 param_group['lr'] = self._learning_rate
 
             print("=======> Oops. Objective was NaN or Inf. Please come again.")
@@ -145,12 +145,12 @@ class Reinforce(object):
 
         # (2) Backpropagate derivatives.
         self._M2_optimizer.zero_grad()
-        self._w2_optimizer.zero_grad()
+        self._f2_optimizer.zero_grad()
         objective.backward()
 
         # (3) Update all learnable parameters.
         self._M2_optimizer.step()
-        self._w2_optimizer.step()
+        self._f2_optimizer.step()
 
     def _generate_v(self):
         """
@@ -160,7 +160,7 @@ class Reinforce(object):
         v = np.empty((self._dynamics.udim, self._num_steps_per_rollout))
         for ii in range(self._dynamics.udim):
             v[ii, :] = np.arange(self._num_steps_per_rollout)
-            v[ii, :] = 1.0 * np.random.uniform(
+            v[ii, :] = np.random.uniform(
                 size=(1, self._num_steps_per_rollout)) * np.sin(
                 2.0 * np.pi * 0.25 * np.random.uniform() * v[ii, :]) + \
                 np.random.normal()
@@ -174,6 +174,7 @@ class Reinforce(object):
                  ``` y(t) = h(x0) + \int \int v(t1) dt1 dt2 ```
         """
         initial_observation = self._dynamics.observation(x0)
+        derivative_initial_observation = self._dynamics.observation_dot(x0)
 #        print("init obs: ", initial_observation)
 
         v_array = np.concatenate(v, axis=1)
@@ -183,7 +184,7 @@ class Reinforce(object):
 #        print("v", v_array)
 
         single_integrated_v = self._dynamics._time_step * np.cumsum(
-            v_array, axis=1)
+            v_array, axis=1) + derivative_initial_observation
 #        double_integrated_v = np.cumsum(
 #            single_integrated_v, axis=1)
         double_integrated_v = self._dynamics._time_step * np.cumsum(
@@ -195,6 +196,7 @@ class Reinforce(object):
         y = initial_observation + double_integrated_v[:, 1:]
         return np.split(
             y, indices_or_sections=self._num_steps_per_rollout, axis=1)
+
 
     def _reward(self, y_desired, y):
         return -np.linalg.norm(y_desired - y)**2

sandbox/test_feedback_linearization.py
@@ -6,7 +6,7 @@ import numpy as np
 from feedback_linearization import FeedbackLinearization
 from double_pendulum import DoublePendulum
 
-dyn = DoublePendulum(1.0, 1.0, 1.0, 1.0)
+dyn = DoublePendulum(1.0, 1.0, 1.0, 1.0, time_step=0.01)
 fb = FeedbackLinearization(dyn, 3, 5, torch.nn.ReLU(), 0.1)
 
 u = np.array([1.0, 1.0])
@@ -35,19 +35,17 @@ v = np.array([1.0, 1.0])
 
 # Generate v, y desired and check that we can match with no model mismatch.
 from reinforce import Reinforce
-r = Reinforce(1, 1, 1, 1, 10, dyn, None, fb)
-current_x = 0.1 * np.ones((4, 1))
+r = Reinforce(1, 1, 1, 1, 100, dyn, None, fb)
+current_x = np.array([[0.1], [0.0], [0.1], [0.0]])
 vs = r._generate_v()
 y_desireds = r._generate_y(current_x, vs)
 
 ys = []
 print("current x: ", current_x)
 for v, y_desired in zip(vs, y_desireds):
-    print(current_x)
+    u = dyn._f_q(current_x) + dyn._M_q(current_x) @ v
+    current_x = dyn.integrate(current_x, u, dt=0.001)
     ys.append(dyn.observation(current_x))
-
-    u = dyn._M_q(current_x) @ dyn._f_q(current_x) + v
-    current_x = dyn.integrate(current_x, u)
 
 print("ys: ", ys)
 print("y_desireds:", y_desireds)
a327328aa1323b56ee4c73aa2b4f7e062a9ce2f5
David Fridovich-Keil <dfk@berkeley.edu>
2019-07-15 12:00:23 -0700

    adding skeleton of new files for ROS/gym stuff

---
+++ ros/src/quads/src/quadrotor_14d_env.py
@@ -0,0 +1,23 @@
+import numpy as np
+import gym
+
+class Quadrotor14dEnv(gym.Env):
+    def __init__(self):
+        # TODO!
+        pass
+
+    def step(self, u):
+        # TODO!
+        pass
+
+    def reset(self):
+        # TODO!
+        pass
+
+    def seed(self, s):
+        # TODO!
+        pass
+
+    def render(self):
+        # TODO!
+        pass

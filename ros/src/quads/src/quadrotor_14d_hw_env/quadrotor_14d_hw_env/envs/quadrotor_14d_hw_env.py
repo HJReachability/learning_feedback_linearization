4786fc6e3aee4719ae89d2265aac09e9b6b6c1ff
HySys <berkeleyhybridsystems@gmail.com>
2019-08-16 10:57:57 -0700

    environment class now resets linear system state in state estimator, which is also computing rewards n stuff before publishing transitions

--- ros/src/quads/src/quadrotor_14d_hw_env/quadrotor_14d_hw_env/envs/quadrotor_14d_hw_env.py
+++ ros/src/quads/src/quadrotor_14d_hw_env/quadrotor_14d_hw_env/envs/quadrotor_14d_hw_env.py
@@ -4,6 +4,7 @@ import sys
 import rospy
 
 from quads_msgs.msg import Transition
+from std_msgs.msg import Empty
 
 class Quadrotor14dHwEnv(gym.Env):
     def __init__(self):
@@ -27,13 +28,34 @@ class Quadrotor14dHwEnv(gym.Env):
             return None, None, None, None, True
 
         transition = self._transitions.pop(0)
-        x = np.array(transition.x)
-#        v = transition.v
-#        u = transition.u
+        x = np.array([transition.x.x, transition.x.y,
+                      transition.x.z, transition.x.theta,
+                      transition.x.phi, transition.x.psi,
+                      transition.x.dx, transition.x.dy,
+                      transition.x.dz, transition.x.zeta,
+                      transition.x.xi, transition.x.q,
+                      transition.x.r, transition.x.p])
+        u = np.array([transition.u.thrustdot2, transition.u.pitchdot2,
+                      transition.u.rolldot2, transition.u.yawdot2])
         r = transition.r
-        return x, r, False, {}
+        return x, r, u, False, {}
+
+    def preprocess_state(self, x):
+        x[0] = np.sin(x[3])
+        x[1] = np.sin(x[4])
+        x[2]= np.sin(x[5])
+        x[3] = np.cos(x[3])
+        x[4] = np.cos(x[4])
+        x[5]= np.cos(x[5])
+
+        # Remove xi.
+        x.pop(10)
+
+        # TODO: think about removing p, q, r?
+        return x
 
     def reset(self):
+        self._linear_system_reset_pub.publish(Empty())
         self.clear()
 
     def render(self):
@@ -49,11 +71,17 @@ class Quadrotor14dHwEnv(gym.Env):
             return False
         self._transition_topic = rospy.get_param("~topics/transition")
 
+        if not rospy.has_param("~topics/linear_system_reset"):
+            return False
+        self._linear_system_reset_topic = rospy.get_param("~topics/linear_system_reset")
+
         return True
 
     def register_callbacks(self):
         self._transition_sub = rospy.Subscriber(
             self._transition_topic, StateTransition, self.transition_callback)
+
+        self._linear_system_reset_pub = rospy.Publisher(self._linear_system_reset_topic, Empty)
 
         return True
 

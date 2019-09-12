25b43c737e46cfbe97d872e02ff0c76370e20d88
Valmik <valmik@berkeley.edu>
2019-09-11 18:57:08 -0700

    IT WORKS (maybe)

--- ros/src/baxter_learning/src/baxter_learning_node.py
+++ ros/src/baxter_learning/src/baxter_learning_node.py
@@ -23,8 +23,8 @@ from std_msgs.msg import Empty
 import utils
 from path_planner import PathPlanner
 
-import spinup2.algos.vpg.core as core
-# import spinup2.algos.ppo.core as core
+# import spinup2.algos.vpg.core as core
+import spinup2.algos.ppo.core as core
 
 
 
@@ -54,11 +54,11 @@ class BaxterLearning():
 
             #define actor critic
             #TODO add in central way to accept arguments
-            # self._pi, self._logp, self._logp_pi, self._v = core.mlp_actor_critic(
-            #    self._x_ph, self._u_ph, hidden_sizes=(64,2), action_space=action_space)
-            POLY_ORDER = 2
-            self._pi, self._logp, self._logp_pi, self._v = core.polynomial_actor_critic(
-                self._x_ph, self._u_ph, POLY_ORDER, action_space=action_space)
+            self._pi, self._logp, self._logp_pi, self._v = core.mlp_actor_critic(
+               self._x_ph, self._u_ph, hidden_sizes=(64,2), action_space=action_space)
+            # POLY_ORDER = 2
+            # self._pi, self._logp, self._logp_pi, self._v = core.polynomial_actor_critic(
+            #     self._x_ph, self._u_ph, POLY_ORDER, action_space=action_space)
 
             #start up tensorflow graph
 

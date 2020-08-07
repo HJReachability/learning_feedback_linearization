972408cfbf34da88957f35d4e916eb0dc9e752b9
valmik <rsnorunt@gmail.com>
2020-08-06 18:08:35 -0700

    wrote install bash script

---
+++ install_requirements.bash
@@ -0,0 +1,10 @@
+pip2 install tensorflow==1.15 --user
+pip2 install cloudpickle --user
+pip2 install gym --user
+pip2 install joblib --user
+pip2 install pandas --user
+pip2 install seaborn==0.8.1 --user
+pip2 install testresources --user
+pip2 install setuptools --upgrade --user
+pip2 install pyasn1==0.4.6 --user
+pip2 install dill --user
